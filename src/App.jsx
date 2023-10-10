5aa37201a39a8117622b3408cad3c4f44b4e6033
Michael <madetunji1376@gmail.com>
2023-10-10 10:01:20 +0100

    files tested to check css framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-
+import React from 'react'
 import './App.css'
 
 function App() {
  
   return (
-   
-      <div className='App'>
-     
-      </div>
-     
-
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
