bdb490d18c4d105aeed2540e1516c84f2e2c7b38
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-10-16 20:34:48 +0100

    revert app jsx design to the original

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import './App.css'
-import WhyChooseUs from './Components/WhyChooseUs'
-import CTA from './Components/CTA'
 
 function App() {
  
   return (
-    // <h1 className="text-3xl font-bold underline">
-    //   Hello world!
-    // </h1>
-    // <WhyChooseUs />
-    <CTA />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
