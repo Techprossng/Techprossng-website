a2d405ffddf36d63d7de35a7b614d5c4391c2ea0
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2023-10-16 13:14:23 +0100

    testimonial and our team

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Testimonial from "./Components/Testimonial/testimonial";
+import OurTeam from "./Components/Our-team/ourTeam";
 
 function App() {
- 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <div className="mx-20 my-16">
+        <Testimonial />
+      </div>
+      <div className="mx-20 my-16">
+        <OurTeam />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
