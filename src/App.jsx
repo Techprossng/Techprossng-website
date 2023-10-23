25f5a01af1799f3ab71a2573121f23d32bb47515
Michael <madetunji1376@gmail.com>
2023-10-23 20:55:20 +0100

    useeffect hook used to run the AOS just once

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import React, { useEffect } from 'react';
 import './App.css'
+import AosInitializer from './assets/aosInitializer';
 import LandingPage from './pages/landing-page';
 
 function App() {
+
+  useEffect(() => {
+    AosInitializer();
+  }, []);
+
+
   return (
      <>
       <LandingPage/>
