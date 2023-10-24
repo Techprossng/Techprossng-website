aebd68d24a426d1c00335ce0d694f65390e2dee3
Michael <madetunji1376@gmail.com>
2023-10-24 02:24:33 +0100

    mobile view worked on and fonts added to the projects

--- src/Components/Landingpage/Services/services.jsx
+++ src/Components/Landingpage/Services/services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
     //i want to create two cards each with an image, title and description
     <section className="services flex flex-col items-center justify-center">
       <div>
-        <h4 className="text-3xl ml-24 mr-24" style={{ color: "#272727" }}>
+        <h4 className="sm:flex items-center md:text-3xl ml-24 mr-24 lg:text-3xl ml-24 mr-24" style={{ color: "#272727" }}>
           OUR SERVICES
         </h4>
         <div
@@ -17,13 +17,13 @@ const Services = () => {
           className="w-16 h-1 rounded-xl ml-24 mr-24 mt-1 mb-3"
         ></div>
       </div>
-      <div className="services-container flex justify-center items-center my-30 gap-20">
-        <div className="card flex flex-col justify-center items-center text-center" data-aos="fade-right">
+      <div className="services-container md:flex justify-center items-center my-30 gap-20 lg:flex justify-center items-center my-30 gap-20">
+        <div className="card lg:flex flex-col justify-center items-center text-center" data-aos="fade-right">
           <img src={online} alt="online-learning" />
-          <h5 className="title text-3xl font-semibold mb-5">
+          <h5 className="title text-3xl font-primary mb-5">
             Online Learning and Training
           </h5>
-          <p className="paragraph text-2xl font-medium">
+          <p className="paragraph text-2xl font-secondary">
             Our online programs offer flexible, up-to-date tech education,
             allowing you to learn at your pace. Engage with practical
             assignments, quizzes, and join a supportive community of learners
@@ -33,8 +33,8 @@ const Services = () => {
 
         <div className="card flex flex-col justify-center items-center text-center" data-aos="fade-left">
           <img src={mentorship} alt="Mentorship" />
-          <h5 className="title text-3xl font-semibold mb-5">Mentorship</h5>
-          <p className="paragraph text-2xl font-medium">
+          <h5 className="title text-3xl font-primary mb-5">Mentorship</h5>
+          <p className="paragraph text-2xl font-secondary">
             Unlock your tech career's full potential through our mentorship
             programs. Connect with experienced professionals for personalized
             guidance, career advice, and ongoing support.
