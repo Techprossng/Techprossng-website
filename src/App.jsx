def4869937e1f95d5f216ad8a29fc03fcd1c930e
Michael <madetunji1376@gmail.com>
2023-10-24 10:46:02 +0100

    the landing branch and home page routes created with some dummy codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import './App.css'
 import AosInitializer from './assets/aosInitializer';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landing-page';
+import Homepage from './pages/homepage/homepage';
 
 function App() {
 
@@ -12,7 +14,10 @@ function App() {
 
   return (
      <>
-      <LandingPage/>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Homepage />} />
+      </Routes>
      </> 
      
   )
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sample from "../../Components/Homepage/Sample/sample";
+
+const Homepage = () => {
+    return (
+        <>
+        <Sample />
+        </>
+    );
+    }
+
+export default Homepage;
