2b0918c5f6a12648d232c856d3ec5877963e8923
Michael <madetunji1376@gmail.com>
2023-10-28 17:24:49 +0100

    changes made according to design

--- src/Components/Landingpage/Mission/mission-vision.jsx
+++ src/Components/Landingpage/Mission/mission-vision.jsx
@@ -2,31 +2,43 @@ import dartImage from "./images/dart-diagram.png";
 import "./styles/mission.css";
 
 const Mission = () => {
-    return (
-        <section className="mission-container flex justify-around">
-            <div className="texts-container flex flex-col justify-center items-center gap-20">
-                <div className="vision-container">
-                    <h4 className="text-heading text-3xl font-primary mb-3 font-normal uppercase">Our Vision</h4>
-                    <hr />
-                    <p className="vision-paragraph text-base font-secondary font-medium">
-                    TechProsNG aims to become Nigeria&apos;s premier EdTech institution by 2030. Our goal is to disseminate tech education throughout the nation, empowering a tech-savvy generation to drive the country&apos;s technological advancement. 
-                    </p>
-                </div>
+  return (
+    <section className="mission-container flex justify-around">
+      <div className="texts-container flex flex-col justify-center items-center gap-20">
+        <div className="absolute top-[140%] left-[1%] z-[-50]">
+          <img
+            src="src/assets/icons/whychooseus_dots.svg"
+            className="w-[185px]"
+          />
+        </div>
+        <div className="vision-container">
+          <h4 className="text-heading text-3xl font-primary mb-3 font-normal uppercase">
+            Our Vision
+          </h4>
+          <hr />
+          <p className="vision-paragraph text-base font-secondary font-medium ">
+            TechProsNG aims to become Nigeria&apos;s premier Tech institution in
+            2030.
+          </p>
+        </div>
 
-                <div className="mission-container" data-aos="fade-up">
-                    <h4 className="text-heading text-3xl font-primary mb-3 font-normal uppercase">Our Mission</h4>
-                    <hr />
-                    <p className="mission-paragraph text-base font-secondary font-medium">
-                    TechProsNG is dedicated to championing digital literacy and tech proficiency in Nigeria&apos;s education and workforce. We offer accessible, relevant tech education to empower students and professionals in an evolving digital landscape, fostering a tech-savvy future, one learner at a time.
-                    </p>
-                </div>    
-            </div>
+        <div className="mission-container" data-aos="fade-up">
+          <h4 className="text-heading text-3xl font-primary mb-3 font-normal uppercase">
+            Our Mission
+          </h4>
+          <hr />
+          <p className="mission-paragraph text-base font-secondary font-medium">
+            TechProsNG is dedicated to championing digital literacy in the near
+            future.
+          </p>
+        </div>
+      </div>
 
-            <div className="mission-image mt-3 mb-5" data-aos="zoom-in">
-                <img src={dartImage} alt="dart image" />
-            </div>
-        </section>
-    )
-}
+      <div className="mission-image mt-3 mb-5" data-aos="zoom-in">
+        <img src={dartImage} alt="dart image" />
+      </div>
+    </section>
+  );
+};
 
 export default Mission;
