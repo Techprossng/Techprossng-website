ecf5fa27a077bad74fe43e0537af0bc3dbf395a0
Michael <madetunji1376@gmail.com>
2023-10-28 23:46:36 +0100

    the right animations have been implemented

--- src/Components/Landingpage/Newsletter Section/newsletter.jsx
+++ src/Components/Landingpage/Newsletter Section/newsletter.jsx
@@ -4,15 +4,15 @@ import '../Newsletter Section/Styles/newsletter.css'
 function Newsletter() {
   return (
         <div>
-        <div class="news-box" data-aos="zoom-in-up">
+        <div className="news-box" data-aos="zoom-in-up">
         <h1> Would you like to stay updated?</h1> 
         <h4>Subscribe to our newsletter.</h4>
-        <div class="email">
-            <input type="email" placeholder="Enter your email address"/>
-            <i class="icon fa fa-envelope"></i>
+        <div className="email" data-aos="fade-right">
+            <input type="email" placeholder="Enter your email address" data-aos="fade-down-left"/>
+            <i className="icon fa fa-envelope"></i>
             <button type="submit">Subscribe</button> 
         </div>
-        <p>We are committed to your privacy. TechPros Ng uses the information you provide to us to contact you about our relevant content, products and services. You may unsubscribe from these communication at any time. For more information, check our <a href="#">Privacy Policy</a></p>
+        <p data-aos="fade-up">We are committed to your privacy. TechPros Ng uses the information you provide to us to contact you about our relevant content, products and services. You may unsubscribe from these communication at any time. For more information, check our <a href="#">Privacy Policy</a></p>
     </div>
     </div>
   )
