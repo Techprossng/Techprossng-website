0f9709869a665ce415b6bdca5489cfd3c95ada49
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-10-29 18:10:08 +0100

    Resource Library completed and ready for review

--- src/Components/Homepage/Resource Library/library.jsx
+++ src/Components/Homepage/Resource Library/library.jsx
@@ -3,25 +3,25 @@ import "../Resource Library/Styles/library.css"
 
 function Library() {
   return (
-    <div class="container">
-        <h1 class="heading">Resource Library <div class="resourse-link"><a href="#">View Resource Library</a></div></h1> 
-        <div class="box-container">
-            <div class="box">
+    <div className="container">
+        <h1 className="heading">Resource Library <div class="resourse-link"><a href="#">View Resource Library</a></div></h1> 
+        <div className="box-container">
+            <div className="box">
                 <img src="../../../src/assets/images/image-library-1.png" alt=""/>
                 <h3>Videos</h3>
                 <a href="#" class="btn">Explore Now</a>
             </div>
-            <div class="box">
+            <div className="box">
                 <img src="../../../src/assets/images/image-library-4.png" alt="" />
                 <h3>E-Books</h3>
                 <a href="#" class="btn">Explore Now</a>
             </div>
-            <div class="box">
+            <div className="box">
                 <img src="../../../src/assets/images/image-library-5.png" alt=""/>
                 <h3>Projects</h3>
                 <a href="#" class="btn">Explore Now</a>
             </div>
-            <div class="box">
+            <div className="box">
                 <img src="../../../src/assets/images/image-library-6.png" alt=""/>
                 <h3>Tutorials</h3>
                 <a href="#" class="btn">Explore Now</a>
