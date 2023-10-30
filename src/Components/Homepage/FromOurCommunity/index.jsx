b40d167fcd865892457f138c088846f86fbd4bcf
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-10-30 20:28:19 +0100

    edited some breakpoints

--- src/Components/Homepage/FromOurCommunity/index.jsx
+++ src/Components/Homepage/FromOurCommunity/index.jsx
@@ -14,9 +14,9 @@ export default function FromOurCommunity() {
       </header>
       <section className="max-[768px]:flex max-[768px]:overflow-hidden max-[768px]:overflow-x-scroll no-scrollbar max-[768px]:w-[150%]" >
         <div className="grid grid-cols-2 place-items-center gap-[32px] max-[768px]:flex max-[768px]:flex-nowrap max-[768px]:flex-shrink-0">
-          <div className="bg-[#2B3F8C] rounded-[16px] min-h-[180px] md:max-w-[559px] max-w-[283px] flex max-[480px]:flex-col items-start gap-[16px]">
-            <img src="src/assets/images/homepage_foc.png" className="md:w-[150px] max-h-[198px] w-full md:p-[16px] rounded-t-[16px] md:rounded-[0px] p-0 object-cover" />
-            <div className="font-secondary text-white space-y-[16px] w-full p-[16px] pt-0">
+          <div className="bg-[#2B3F8C] rounded-[16px] min-h-[180px] sm:max-w-[559px] max-w-[583px] flex max-[480px]:flex-col items-start gap-[16px]">
+            <img src="src/assets/images/homepage_foc.png" className="sm:w-[150px] max-h-[198px] w-full md:p-[16px] rounded-t-[16px] md:rounded-[0px] p-0 object-cover" />
+            <div className="font-secondary text-white space-y-[16px] w-full p-[16px]">
               <p className="font-bold">Article Title</p>
               <p className="text-[12px] overflow-ellipsis overflow-hidden w-[100%] h-[53px] text-[#E9E9E9]">
               This area contains the description of the article. This area contains the description of the article. This area contains the description of the article.This area contains the description of the article.
@@ -35,7 +35,69 @@ export default function FromOurCommunity() {
               </div>
             </div>
           </div>
-
+          <div className="bg-[#2B3F8C] rounded-[16px] min-h-[180px] sm:max-w-[559px] max-w-[283px] flex max-[480px]:flex-col items-start gap-[16px]">
+            <img src="src/assets/images/homepage_foc.png" className="sm:w-[150px] max-h-[198px] w-full md:p-[16px] rounded-t-[16px] md:rounded-[0px] p-0 object-cover" />
+            <div className="font-secondary text-white space-y-[16px] w-full p-[16px]">
+              <p className="font-bold">Article Title</p>
+              <p className="text-[12px] overflow-ellipsis overflow-hidden w-[100%] h-[53px] text-[#E9E9E9]">
+              This area contains the description of the article. This area contains the description of the article. This area contains the description of the article.This area contains the description of the article.
+              </p>
+              <div className="flex justify-between items-end">
+                <div className="flex gap-x-[8px] items-center">
+                  <img src="src/assets/images/homepage_foc_image.png" width={32} height={32}/>
+                  <div className="space-y-[4px] ">
+                    <p className="text-[12px] font-bold">Daniel Mbwangu</p>
+                    <p className="text-[11px] font-medium">3 mins read</p>
+                  </div>
+                </div>
+                <p className="text-[11px] font-medium">
+                  Aug 20, 2023
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#2B3F8C] rounded-[16px] min-h-[180px] sm:max-w-[559px] max-w-[283px] flex max-[480px]:flex-col items-start gap-[16px]">
+            <img src="src/assets/images/homepage_foc.png" className="sm:w-[150px] max-h-[198px] w-full md:p-[16px] rounded-t-[16px] md:rounded-[0px] p-0 object-cover" />
+            <div className="font-secondary text-white space-y-[16px] w-full p-[16px]">
+              <p className="font-bold">Article Title</p>
+              <p className="text-[12px] overflow-ellipsis overflow-hidden w-[100%] h-[53px] text-[#E9E9E9]">
+              This area contains the description of the article. This area contains the description of the article. This area contains the description of the article.This area contains the description of the article.
+              </p>
+              <div className="flex justify-between items-end">
+                <div className="flex gap-x-[8px] items-center">
+                  <img src="src/assets/images/homepage_foc_image.png" width={32} height={32}/>
+                  <div className="space-y-[4px] ">
+                    <p className="text-[12px] font-bold">Daniel Mbwangu</p>
+                    <p className="text-[11px] font-medium">3 mins read</p>
+                  </div>
+                </div>
+                <p className="text-[11px] font-medium">
+                  Aug 20, 2023
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#2B3F8C] rounded-[16px] min-h-[180px] sm:max-w-[559px] max-w-[283px] flex max-[480px]:flex-col items-start gap-[16px]">
+            <img src="src/assets/images/homepage_foc.png" className="sm:w-[150px] max-h-[198px] w-full sm:p-[16px] rounded-t-[16px] md:rounded-[0px] p-0 object-cover" />
+            <div className="font-secondary text-white space-y-[16px] w-full p-[16px]">
+              <p className="font-bold">Article Title</p>
+              <p className="text-[12px] overflow-ellipsis overflow-hidden w-[100%] h-[53px] text-[#E9E9E9]">
+              This area contains the description of the article. This area contains the description of the article. This area contains the description of the article.This area contains the description of the article.
+              </p>
+              <div className="flex justify-between items-end">
+                <div className="flex gap-x-[8px] items-center">
+                  <img src="src/assets/images/homepage_foc_image.png" width={32} height={32}/>
+                  <div className="space-y-[4px] ">
+                    <p className="text-[12px] font-bold">Daniel Mbwangu</p>
+                    <p className="text-[11px] font-medium">3 mins read</p>
+                  </div>
+                </div>
+                <p className="text-[11px] font-medium">
+                  Aug 20, 2023
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </section>
