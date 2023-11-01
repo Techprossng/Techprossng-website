0d4ed7d6af9d151f8e55bf6e479f00d807973129
Benjamin fadipe <fadipemoyinoluwa2016@gmail.com>
2023-11-01 04:38:32 +0100

    adjusted the hero section , so it can be inline with the figma file

--- src/Components/Landingpage/Footer/footer.jsx
+++ src/Components/Landingpage/Footer/footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../Footer/Styles/footer.css";
 
 function Footer() {
--- src/Components/Landingpage/Hero section/hero.jsx
+++ src/Components/Landingpage/Hero section/hero.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import HeroImage from "../../../assets/images/hero-image.svg";
+import "../../Landingpage/Hero Section/Styles/hero.css";
 import "aos";
 
 const Hero = () => {
   return (
-    <div className="px-6 py-12 md:px-12 text-center lg:text-left  mt-24">
+    <div className="px-6 py-12 md:px-12 text-center lg:text-left  mt-24 hero-background"
+    >
       <div className="container mx-auto xl:px-32">
         <div className="grid lg:grid-cols-2 gap-12 flex items-center">
           <div
@@ -13,7 +14,13 @@ const Hero = () => {
             data-aos="fade-right"
             data-aos-duration="2000"
           >
-            <h1 className="text-5xl flex font-primary md:text-4xl font-primary xl:font-primary text-6xl font-semibold tracking-tight mb-12 text-[#001975] animate-text text-5xl">
+            <img  src="../../src/assets/images/Vector.svg" className="Vector-img"/>
+            <h1 className=" text-6xl md:text-6xl lg:text-5xl xl:text-7xl flex font-primary font-primary xl:font-primary  font-bold tracking-tight mb-12 text-[#001975] animate-text "
+             style={{
+           
+            fontWeight:"900px"
+          
+            }}>
               <br />
               TechProsNG
             </h1>
@@ -21,11 +28,18 @@ const Hero = () => {
               We are Tech Education Company dedicated to empowering the next
               generation of tech innovators in Nigeria. Explore our cutting-edge
               courses, join a supportive tech community, and become confident in
-              your tech skills. Together we're driving progress in the digital
+              your tech skills. Together we are driving progress in the digital
               era. Start your transformative journey with us now.
             </p>
 
-            <button class="inline-flex hidden sm:hidden md:hidden lg:block items-center px-6 py-2 bg-transparent hover:bg-blue-900 text-red-500 font-semibold hover:text-white py-2 px-8 border border-red-700 hover:border-transparent rounded-full mt-5">
+            <button className="flex hidden sm:hidden md:hidden lg:flex justify-between items-center px-10 py-5 bg-transparent hover:bg-blue-900 text-red-500 font-semibold border-2 border-red-500 transition duration-300 hover:border-blue-900 hover:text-white  mt-5"
+            style={{
+              width: "220px",
+              borderRadius: "20px",
+              fontSize: "14px",
+          
+            }}>
+          
               <span>See Courses</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +64,7 @@ const Hero = () => {
           >
             <img
               src={HeroImage}
-              className="w-full rounded-lg shadow-lg transition duration-500 ease-in-out bg-white-700 hover:bg-blue-900 transform hover:-translate-y-1 hover:scale-100 "
+              className="w-full rounded-lg  transition duration-500 ease-in-out bg-white-700 transform hover:-translate-y-1 hover:scale-100 "
               alt=""
             />
           </div>
