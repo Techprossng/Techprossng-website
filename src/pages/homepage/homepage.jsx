c98ee07b48eae5945a1011cfdf4468d684e31c59
Benjamin fadipe <fadipemoyinoluwa2016@gmail.com>
2023-11-02 12:19:29 +0100

    commit

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,6 +1,6 @@
 
 import Sample from "../../Components/Homepage/Sample/sample";
-import Navbar from "../../Components/Landingpage/Navbar/navbar";
+import Navbar from "../../Components/Homepage/Navbar/navbar";
 import Mentor from "../../Components/Homepage/Mentor/mentor";
 
 
