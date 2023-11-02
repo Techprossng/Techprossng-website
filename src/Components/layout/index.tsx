6d13a9288ee2a3b0c49efc1d0f797462697ad44e
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2023-11-02 12:13:18 +0100

    second commit

--- src/Components/layout/index.tsx
+++ src/Components/layout/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Navbar from "../Landingpage/Navbar/navbar";
 
 interface LayoutProps {
@@ -9,7 +8,7 @@ interface LayoutProps {
 export default function Layout({children}: LayoutProps) {
   // Put Header or Footer Here
   return (
-    <main className="container mx-auto ">
+    <main className="container mx-auto max-w-20xl">
       <Navbar />
       <div>{children}</div>
     </main>
