66e6e69ace51e697b3e2cb02519b09806e034783
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2023-11-03 10:27:01 +0100

    mody3

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,65 @@
-import  { useEffect } from 'react';
-import './App.css'
-import AosInitializer from './assets/aosInitializer';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/landing-page';
-import Homepage from './pages/homepage/homepage';
-import Layout from './Components/layout';
-import Profilepage from './pages/Profilepage/profilepage/';
+import { useEffect } from "react";
+import "./App.css";
+import AosInitializer from "./assets/aosInitializer";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing-page";
+import Homepage from "./pages/homepage/homepage";
+import Layout from "./Components/layout";
+import Profilepage from "./pages/Profilepage/profilepage/";
 import Pricing from "./pages/Pricing/pricing";
-import Catalog from './pages/Coursecatalog/course';
+import Catalog from "./pages/Coursecatalog/course";
 
 function App() {
-
   useEffect(() => {
     AosInitializer();
   }, []);
 
-
   return (
-     <>
+    <>
       <Routes>
-        <Route path="/" element={<Layout><LandingPage /></Layout>} />
-        <Route path="/home" element={<Layout><Homepage /></Layout>} /> 
-        <Route path="/profile" element={<Layout><Profilepage/></Layout>} />
-        <Route path="/pricing" element={<Layout><Pricing /></Layout>} />
-        <Route path="/course-catalog" element={<Layout><Catalog /></Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <LandingPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <Layout>
+              <Homepage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <Profilepage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/pricing"
+          element={
+            <Layout>
+              <Pricing />
+            </Layout>
+          }
+        />
+        <Route
+          path="/course-catalog"
+          element={
+            <Layout>
+              <Catalog />
+            </Layout>
+          }
+        />
       </Routes>
-     </> 
-  )
+    </>
+  );
 }
 
 export default App;
