3bb46c8f3656a1457b95960e7e01cc7b00876c2f
Michael <madetunji1376@gmail.com>
2023-11-04 09:21:05 +0100

    deleting sample file

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,5 +1,3 @@
-
-import Sample from "../../Components/Homepage/Sample/sample";
 import Navbar from "../../Components/Homepage/Navbar/navbar";
 import Mentor from "../../Components/Homepage/Mentor/mentor";
 
@@ -9,7 +7,6 @@ const Homepage = () => {
         <>
         <Navbar/>
         <Mentor/>
-        <Sample />
         </>
     );
     }
