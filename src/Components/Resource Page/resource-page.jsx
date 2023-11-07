fcbd0f73b8ab5a63431f55673b2d48128e157686
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-11-07 23:47:49 +0100

    Resource Page desktop view

--- src/Components/Resource Page/resource-page.jsx
+++ src/Components/Resource Page/resource-page.jsx
@@ -47,6 +47,64 @@ const Resource = () => {
                       <h5>Find guidance on changing your password, updating your profile, and managing your account.</h5>
                   </div>
                 </div>
+                <div className="faq-link">
+                  <a href="#">Click here to visit the FAQ page.</a>
+                </div>
+          </div>
+          <div className="contact-us">
+            <h1>Contact Us</h1>
+              <h6>If you can't find the answer you're looking for on our FAQ page, our dedicated support team is ready to assist you. You can reach us through any of the following channels:</h6>
+                <div className="col3">
+                  <div className="tab">
+                    <h2>Email Support</h2>
+                      <h6>Send an email to support@techprosng.com with your inquiry, and our team will respond promptly.</h6>
+                  </div>
+                  <div className="tab">
+                    <h2>Live Chat</h2>
+                      <h6>Our live chat support is available from 9am - 3pm, monday to friday to provide real-time assistance.</h6>
+                  </div>
+                  <div className="tab">
+                    <h2>Phone Support</h2>
+                      <h6>Call our support hotline at +2349017433227 if you prefer to speak with a representative directly.</h6>
+                  </div>
+                </div>
+          </div>
+          <div className="guide">
+            <h1>User Guide and Resources</h1>
+              <h6>Explore our library of user guides and resources to make the most of your learning journey:</h6>
+                <div className="col4">
+                  <div className="tab">
+                    <h2>Getting Started Guide</h2>
+                      <h6>New to Tech Pros Ng? Check our guide for a smooth start.</h6>
+                  </div>
+                  <div className="tab">
+                    <h2>Technical Troubleshooting</h2>
+                      <h6>Our live chat support is available from 9am - 3pm to provide real-time assistance.</h6>
+                  </div>
+                  <div className="tab">
+                    <h2>Learning Process</h2>
+                      <h6>Discover additional resources to enhance your understanding of the course material.</h6>
+                  </div>
+                </div>
+                <div className="col5">
+                <div className="tab">
+                    <h2>Tips for Success</h2>
+                      <h6>Get tips and strategies for successful online learning.</h6>
+                  </div>
+                </div>
+          </div>
+          <div className="bottom">
+            <div className="feedback">
+              <h1>Feedback and Suggestions</h1>
+              <h6>We value your feedback. If you have suggestions, ideas, or if you've encountered any issues, please let us know. We are committed to continuous improvement, and your input is essential to our growth.</h6>
+            </div>
+            <div className="community-forum">
+              <h1>Community Forum</h1>
+              <h6>Connect with fellow learners, share experiences, and seek advice on our community forum. It's a great place to collaborate, discuss course topics, and find study partners.</h6>
+            </div>
+          </div>
+          <div className="closing">
+            <h2>Thank you for choosing TechPros Ng as your learning partner. We're here to support you every step of the way on your educational journey.</h2>
           </div>
         </div>
       </>
