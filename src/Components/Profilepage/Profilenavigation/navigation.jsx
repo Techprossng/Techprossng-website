0f6b301f7c57d070a01ad98fceef661fedfdfcf2
Benjamin fadipe <fadipemoyinoluwa2016@gmail.com>
2023-11-07 19:51:17 +0100

    made some changes to the navbar(homepage/landing) , added the side-menu for the profile-page , adjusted the responsiveness on mentors section across all devices

--- src/Components/Profilepage/Profilenavigation/navigation.jsx
+++ src/Components/Profilepage/Profilenavigation/navigation.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import { useState } from "react";
+import {
+  BiHome,
+  BiGroup,
+  BiImages,
+  BiLibrary,
+  BiFace,
+  BiUser,
+  BiSearch,
+} from "react-icons/bi";
+import { AiOutlineSetting } from "react-icons/ai";
+import { GoSignOut } from "react-icons/go";
 
 const Navigation = () => {
   let Links = [
@@ -10,7 +21,25 @@ const Navigation = () => {
     { name: "Mentors", link: "/mentorship" },
   ];
 
+  let Links2 = [
+    { name: "Search", link: "/search" },
+    { name: "Home", link: "/" },
+    { name: "Catalog", link: "/catalog" },
+    { name: "Library", link: "/library" },
+    { name: "Community", link: "/community" },
+    { name: "Mentors", link: "/mentors" },
+    { name: "Profile", link: "/profile" },
+    { name: "Settings", link: "/settings" },
+    { name: "Logout", link: "/logout" },
+  ];
+ 
   let [open, setOpen] = useState(false);
+   
+  const [activeLink, setActiveLink] = useState("Home"); // Set the default active link
+
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
 
 
   return (
@@ -32,46 +61,86 @@ const Navigation = () => {
       </div>
 
       {/* Mobile Menu Button */}
-      <div
-        onClick={() => setOpen(!open)}
-        className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-      >
-        <img
-          src={
-            open ? "src/assets/icons/close.svg" : "src/assets/icons/menu.svg"
-          }
-          alt={open ? "Close Menu" : "Open Menu"}
-          className="menu"
-        />
+      <div className="text-3xl absolute right-8 top-6 cursor-pointer menu-container md:hidden">
+        {!open && (
+          <img
+            src="src/assets/icons/menu.svg"
+            alt="Open-Menu"
+            className="menu"
+            onClick={() => setOpen(true)}
+          />
+        )}
       </div>
 
       {/* Mobile Menu */}
 
+      
       <div
-        className={`md:hidden 
-  md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-0 pt-10 transition-all duration-500 ease-in menu-color ${
-    open ? "top-24 " : "top-[-690px]"
-  }`}
-        style={{ textAlign: "center", alignItems: "center" }}
+        className={`md:hidden md:pb-0 pb-12  absolute md:static md:z-auto right-0 w-full h-screen md:w-auto md:pl-0 pl-0 pt-10 transition-all duration-500 ease-in menu-color mobile-menu ${
+          open ? "top-0 translate-x-0" : "top-0 translate-x-full"
+        }`}
+        style={{ borderRadius: "8px", maxHeight: "100vh", overflowY: "scroll"  }}
       >
-        <div className="flex flex-col space-y-10">
-          {Links.map((link) => (
+        <div className="text-3xl relative pl-7 mt-10 right-0 top-0 cursor-pointer menu-container">
+          {open && (
+            <img
+              src="src/assets/icons/next.png"
+              alt="Close-Menu"
+              className="menu-back"
+              onClick={() => setOpen(false)}
+            />
+          )}
+        </div>
+
+        <div className="flex flex-col  custom-space-y   mt-20">
+          {Links2.map((link) => (
             <a
               key={link.name}
               href={`/${link.link}`}
-              className={`text-gray-800  duration-500`}
-              style={{ fontSize: "20px", fontWeight: "500" }}>
+              className={`text-blue-900 flex duration-500  p-8  ${
+                activeLink === link.name ? "active-link" : ""
+              } ${link.name.toLowerCase().replace(/\s/g, "-")}`}
+              style={{
+                fontSize: "19px",
+                fontWeight: "700",
+                border: "1px solid rgba(204, 209, 227, 1)",
+                
+              }}
+              onClick={(e) => {
+                e.preventDefault();
+                handleLinkClick(link.name);
+              }}
+            >
+              {link.name === "Search" && (
+                <BiSearch size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Home" && (
+                <BiHome size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Catalog" && (
+                <BiImages size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Library" && (
+                <BiLibrary size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Community" && (
+                <BiGroup size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Mentors" && (
+                <BiFace size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Profile" && (
+                <BiUser size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Settings" && (
+                <AiOutlineSetting size={25} className="mr-5 p-0" />
+              )}
+              {link.name === "Logout" && (
+                <GoSignOut size={25} className="mr-5 p-0" />
+              )}
               {link.name}
             </a>
           ))}
-        </div>
-        <div className=" flex flex-wrap space-y-4 pr-5 pl-5 mt-10 ">
-          <button className="w-full h-[48px] px-5 py-3 rounded-md  button-2-small">
-            Log in
-          </button>
-          <button className="w-full h-[48px] px-5 py-3 rounded-md  text-white button-1">
-            Signup
-          </button>
         </div>
       </div>
 
