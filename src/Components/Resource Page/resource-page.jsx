7644a34b75c949348fd9f984b9c9eac988f8458c
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-11-07 11:47:06 +0100

    Resource Page component created

---
+++ src/Components/Resource Page/resource-page.jsx
@@ -0,0 +1,18 @@
+import "../Resource Page/Style/resource.css";
+const Resource = () => {
+     return (
+       <>
+         <div className="resource">
+           <h1>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vel
+             voluptas architecto laboriosam? Sed incidunt aspernatur quisquam fugit
+             tempora. Libero, magni. Qui quas ratione quae error minus eaque
+             dolore? Quibusdam.
+           </h1>
+         </div>
+       </>
+     );
+   };
+   
+   export default Resource;
+   
