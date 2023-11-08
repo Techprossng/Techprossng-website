656b2675fc5e4e6eb68eeb409d0edce2eb3c7ebe
Michael <madetunji1376@gmail.com>
2023-11-08 18:45:02 +0100

    dashboard design being worked on

--- src/Components/Profilepage/Dashboard/dashboard.jsx
+++ src/Components/Profilepage/Dashboard/dashboard.jsx
@@ -1,35 +1,73 @@
-import React from 'react';
-import Navigation from '../Profilenavigation/navigation';
-import Sidebar from '../Profilenavigation/sidebar';
-
+import React from "react";
+import Navigation from "../Profilenavigation/navigation";
+import Sidebar from "../Profilenavigation/sidebar";
+import person from "../../../assets/icons/person.svg";
+import "../Profilenavigation/styles/style.css";
 const Dashboard = () => {
   return (
-    <section>
-    <Navigation />
+    <section className="flex w-full">
+      <Navigation />
 
-    {/* path change */}
+      {/* path change */}
 
-    <div className='bg-[aqua] mt-24 border'>
-        <div className='border-r-1 w-1/5 bg-[red] flex justify-center'>
-            <h1>Dashboard</h1>
-        </div>
-    </div>
-    {/* sidebar */}
+      {/* sidebar */}
 
-    <div className='bg-[#fff] flex justify-end border border-r-1 w-1/6 h-screen'>
+      <div className="bg-[#fff] flex justify-end border border-r-1 w-1/6 h-screen mt-24">
         <Sidebar />
-    </div>
-    
+      </div>
 
+      {/* main content */}
 
-    {/* main content */}
+      <div className="flex grow mt-24 flex-col bg-[grey]">
+        <div className="first-border-box">
+          <h1>Dashboard</h1>
+        </div>
 
-    {/* footer */}
+        <div className="dashboard-box">
+          <main className="dashboard-grid">
+            <div className="columns one grid-col-span-2">
+              <div className="title-border bg-[#fff] flex flex-col gap-y-6">
+                <div className="flex gap-3">
+                  <img src={person} alt="personal-info" />
+                  <h1>Personal Information</h1>
+                </div>
+                <div className="flex gap-3">
+                  <div className="social-icons">
+                    <i className="fa-brands fa-discord"></i>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <h5 className="font-semibold text-xl">Bukayo Saka</h5>
+                    <p>
+                      This area contains Bio. This area contains Bio. This area
+                      contains Bio. This area contains Bio. This area contains
+                      Bio. This area contains Bio. This area contains Bio. This
+                      area contains Bio. This area contains Bio.
+                    </p>
+                    <div className="flex">
+                      <div className="w-[50%]">
+                        <h5>Phone Number</h5>
+                      </div>
+                      <div className="w-[50%]">
+                        <h5>Email Address</h5>
+                      </div>
+                    </div>
+                    <h5>Country</h5>
+                    <h5>Date Joined</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="columns two bg-[red]">2</div>
+            <div className="columns three bg-[red]">3</div>
+            <div className="columns four bg-[red]">4</div>
+            <div className="columns five bg-[red] ">5</div>
+          </main>
+        </div>
+      </div>
 
-
-
+      {/* footer */}
     </section>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/Components/Profilepage/Profilenavigation/sidebar.jsx
+++ src/Components/Profilepage/Profilenavigation/sidebar.jsx
@@ -1,16 +1,19 @@
 //array for the sidebar
 import React from "react";
-import dashboardicon from "../../../assets/icons/dashboardicon.png";
-// import personal from "../../../assets/icons/profile-person.svg";
-// import courses from "../../../assets/icons/profile-book.svg";
-// import achievements from "../../../assets/icons/profile-hotel-class.svg";
+import { MdOutlineDashboard } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
+import { AiOutlineBook } from "react-icons/ai";
+import { GrAchievement } from "react-icons/gr";
+import { LuSettings } from "react-icons/lu";
+
+// const dashboardicon = <FaBeer />;
 
 const sidebar = [
-  { icon: dashboardicon, title: "Dashboard" },
-//   { icon: personal, title: "Personal" },
-//   { icon: courses, title: "Courses" },
-//   { icon: achievements, title: "Achievements" },
-//   { icon: dashboardicon, title: "Settings" },
+  { icon: <MdOutlineDashboard />, title: "Dashboard" },
+  { icon: <BsPerson />, title: "Personal" },
+  { icon: <AiOutlineBook />, title: "Courses" },
+  { icon: <GrAchievement />, title: "Achievements" },
+  { icon: <LuSettings />, title: "Settings" },
 ];
 
 const Sidebar = () => {
@@ -20,12 +23,17 @@ const Sidebar = () => {
         return (
           <div
             key={index}
-            className="sidebar-item flex flex-col items-center py-12 px-5"
+            className="sidebar-item flex flex-col items-center py-10 px-3 space-y-2"
           >
             <div className="sidebar-image">
-                <i className="color-[red]">
-                    <img src={item.icon} alt="icon" />
-                </i>
+              <i
+                style={{
+                  color: "#001975",
+                  fontSize: "20px",
+                }}
+              >
+                {item.icon}
+              </i>
             </div>
             <div className="text-[#001975]">
               <p>{item.title}</p>
