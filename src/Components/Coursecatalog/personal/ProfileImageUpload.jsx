93e943954831ec2a33feb373f249bde6929bd5b3
Benjamin Fadipe <fadipemoyinoluwa2016@gmail.com>
2023-11-09 15:01:01 +0100

    commit

--- src/Components/Coursecatalog/personal/ProfileImageUpload.jsx
+++ src/Components/Coursecatalog/personal/ProfileImageUpload.jsx
@@ -1,34 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import "../personal/Styles/profileimage.css"
+
 
 function ProfileImageUpload({ userData, onSaveUserInfo }) {
-  // Implement image upload logic and set the image in userData
-  const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-    // Handle the file upload and set the image in userData
-    onSaveUserInfo({
-      ...userData,
-      profileImage: URL.createObjectURL(file),
-    });
-  };
-
-  return (
-    <div className="mb-4">
-      <h2 className="text-lg mb-2">Profile Image</h2>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageUpload}
-        className="mb-2"
-      />
-      {userData.profileImage && (
-        <img
-          src={userData.profileImage}
-          alt="Profile"
-          className="w-24 h-24 rounded-full"
+    const [loadingProgress, setLoadingProgress] = useState(0);
+    const [blurAmount, setBlurAmount] = useState(80);
+  
+    const handleImageUpload = (e) => {
+      const file = e.target.files[0];
+      setLoadingProgress(0);
+  
+      const img = new Image();
+      img.onload = () => {
+        setLoadingProgress(100);
+        setBlurAmount(0);
+        onSaveUserInfo({
+          ...userData,
+          profileImage: URL.createObjectURL(file),
+        });
+      };
+      img.src = URL.createObjectURL(file);
+  
+      updateProgressAndBlur();
+    };
+  
+    const updateProgressAndBlur = () => {
+      let startTimestamp = null;
+  
+      const animateProgressAndBlur = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / 1000, 1) * 100;
+        const blur = Math.max(80 - progress, 0);
+  
+        setLoadingProgress(progress);
+        setBlurAmount(blur);
+  
+        if (progress < 100) {
+          requestAnimationFrame(animateProgressAndBlur);
+        }
+      };
+  
+      requestAnimationFrame(animateProgressAndBlur);
+    };
+  
+    return (
+      <div className="mb-4" 
+      style={{ background:"#fff", width:"100%" , height:"300px"   , boxShadow:"10px 20px 20px grey" }}
+      
+      >
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          className=" p-20 "
+          style={{ zIndex:"-1" }}
+          
         />
-      ) }
-    </div>
-  );
-}
+        <div className="image-container"
+        >
+          {userData.profileImage && (
+            <div className="image-loading-overlay" style={{ opacity: loadingProgress / 10 }}>
+              <div className="circular-progress-bar">
+                <div className="border-circle">
+                  <div
+                    className="fill-circle"
+                    style={{ transform: `rotate(${(loadingProgress / 100) * 360}deg)` }}
+                  ></div>
+                </div>
+                <div className="loading-percentage">
+                  {Math.floor(loadingProgress)}%
+                </div>
+              </div>
+            </div>
+          )}
+          <img
+            src={userData.profileImage}
+            className="image-view"
+            alt="Profile"
+            style={{ filter: `blur(${blurAmount}px)` , borderRadius:"25px" }}
+          />
+        </div>
+      </div>
+    );
+  }
+  
+  export default ProfileImageUpload;
 
-export default ProfileImageUpload;
+  
+  
+  
+  
+  
+  
