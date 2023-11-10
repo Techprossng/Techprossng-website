4566f97eab796d396675b4acfc89c836971ae473
Michael <madetunji1376@gmail.com>
2023-11-10 17:31:38 +0100

    working on calender next and responsiveness

--- src/Components/Profilepage/Dashboard/dashboard.jsx
+++ src/Components/Profilepage/Dashboard/dashboard.jsx
@@ -3,56 +3,52 @@ import Navigation from "../Profilenavigation/navigation";
 import Sidebar from "../Profilenavigation/sidebar";
 import person from "../../../assets/icons/person.svg";
 import book from "../../../assets/icons/book.svg";
+import achievement from "../../../assets/icons/hotel-class.png";
+import star from "../../../assets/icons/star_border.svg";
+import events from "../../../assets/icons/event.svg";
 import "../Profilenavigation/styles/style.css";
 const Dashboard = () => {
   return (
     <section className="flex w-full">
       <Navigation />
-
-      {/* path change */}
-
       {/* sidebar */}
-
       <div className="bg-[#fff] flex justify-end border border-r-1 w-1/6 h-screen mt-24">
         <Sidebar />
       </div>
-
       {/* main content */}
-
-      <div className="flex grow mt-24 flex-col bg-[grey]">
+      <div className="flex grow mt-24 flex-col bg-[#F6F6F6]">
         <div className="first-border-box">
-          <h1>Dashboard</h1>
+          <h1 className="font-bold text-2xl font-primary text-[#00114E]">Overview</h1>
         </div>
-
         <div className="dashboard-box">
           <main className="dashboard-grid">
             <div className="columns one grid-col-span-2">
-              <div className="title-border bg-[#fff] flex flex-col gap-y-6">
+              <div className="parent-border bg-[#fff] flex flex-col gap-y-6">
                 <div className="flex gap-3">
                   <img src={person} alt="personal-info" />
-                  <h1>Personal Information</h1>
+                  <h1 className="text-[#2B3F8C] text-base font-bold font-secondary">Personal Information</h1>
                 </div>
                 <div className="flex gap-3">
                   <div className="social-icons">
                     <i className="fa-brands fa-discord"></i>
                   </div>
                   <div className="flex flex-col gap-3">
-                    <h5 className="font-semibold text-xl">Bukayo Saka</h5>
-                    <p>
+                    <h5 className="text-[#000] text-base font-bold font-secondary">Bukayo Saka</h5>
+                    <p className="text-[#656565] text-base font-medium">
                       This area contains Bio. This area contains Bio. This area
                       contains Bio. This area contains Bio. This area contains
                       Bio. This area contains Bio. This area contains Bio. This
                       area contains Bio. This area contains Bio.
                     </p>
                     <div className="flex">
-                      <div className="w-[50%]">
+                      <div className="w-[50%] text-[#000] font-semibold font-medium font-secondary">
                         <h5>Phone Number</h5>
                       </div>
-                      <div className="w-[50%]">
+                      <div className="w-[50%] text-[#000] font-semibold font-medium font-secondary">
                         <h5>Email Address</h5>
                       </div>
                     </div>
-                    <h5>Country</h5>
+                    <h5 className="text-[#000] font-semibold font-secondary">Country</h5>
                     <h5>Date Joined</h5>
                   </div>
                 </div>
@@ -60,68 +56,76 @@ const Dashboard = () => {
             </div>
             <div className="columns two bg-[red]">2</div>
             <div className="columns three">
-              <div className="title-border bg-[#fff] flex flex-col gap-y-6">
+              <div className="parent-border bg-[#fff] flex flex-col gap-y-6">
                 <div className="flex gap-3">
                   <img src={book} alt="courses" />
-                  <h1>Courses</h1>
+                  <h1 className="text-[#2B3F8C] text-base font-bold font-secondary">Courses</h1>
                 </div>
-                <div className="flex flex-col">
-                  <div className="title-border bg-[#fff] flex justify-between items-center">
+                <div className="flex flex-col gap-y-4">
+                  <div className="parent-border  bg-[#fff] flex justify-between items-center">
                     <div className="flex flex-col">
-                      <h5>Ongoing</h5>
-                      <figure>0</figure>
+                      <h5 className="text-[#000] text-xs font-medium font-secondary">Ongoing</h5>
+                      <figure className="text-[#000] font-bold text-base font-secondary">0</figure>
                     </div>
-                    <figure className="text-4xl font-semibold">0</figure>
+                    <figure className="text-4xl font-semibold font-primary text-[#2B3F8C]">0</figure>
                   </div>
-                  <div className="title-border bg-[#fff] flex justify-between items-center">
+                  <div className="parent-border  bg-[#fff] flex justify-between items-center">
                     <div className="flex flex-col">
-                      <h5>Completed</h5>
-                      <figure>0</figure>
+                      <h5 className="text-[#000] text-xs font-medium font-secondary">Completed</h5>
+                      <figure className="text-[#000] font-bold text-base font-secondary">0</figure>
                     </div>
-                    <figure className="text-4xl font-semibold">0</figure>
+                    <figure className="text-4xl font-semibold font-primary text-[#2B3F8C]">0</figure>
                   </div>
-                  <div className="title-border bg-[#fff] flex justify-between items-center">
+                  <div className="parent-border  bg-[#fff] flex justify-between items-center">
                     <div className="flex flex-col">
-                      <h5>Attendance</h5>
-                      <figure>Total-100%</figure>
+                      <h5 className="text-[#000] text-xs font-medium font-secondary">Attendance</h5>
+                      <figure className="text-[#000] font-bold text-base">Total-100%</figure>
                     </div>
-                    <figure className="text-4xl font-semibold">0</figure>
+                    <figure className="text-4xl font-semibold font-primary text-[#2B3F8C]">0</figure>
                   </div>
                 </div>
               </div>
             </div>
             <div className="columns four">
-            <div className="title-border bg-[#fff] flex flex-col gap-y-6">
+              <div className="parent-border bg-[#fff] flex flex-col gap-y-6">
                 <div className="flex gap-3">
-                  <img src={book} alt="courses" />
-                  <h1>Courses</h1>
+                  <img src={achievement} alt="achievement-icon" />
+                  <h1 className="text-[#2B3F8C] text-base font-bold font-secondary">Achievements</h1>
                 </div>
-                <div className="flex flex-col">
-                  <div className="title-border bg-[#fff] flex justify-between items-center">
+                <div className="flex flex-col gap-y-4">
+                  <div className="parent-border bg-[#fff] flex justify-between items-center">
                     <div className="flex flex-col">
-                      <h5>Ongoing</h5>
-                      <figure>0</figure>
+                      <h5 className="text-[#000] text-xs font-medium font-secondary">Total learning hours</h5>
+                      <figure className="text-[#000] font-bold text-base font-secondary">0 hours</figure>
                     </div>
-                    <figure className="text-4xl font-semibold">0</figure>
+                    <img src={star} alt="star" />
                   </div>
-                  <div className="title-border bg-[#fff] flex justify-between items-center">
+                  <div className="parent-border bg-[#fff] flex justify-between items-center">
                     <div className="flex flex-col">
-                      <h5>Completed</h5>
-                      <figure>0</figure>
+                      <h5 className="text-[#000] text-xs font-medium font-secondary">Certificates completed</h5>
+                      <figure className="text-[#000] font-bold text-base font-secondary">0</figure>
                     </div>
-                    <figure className="text-4xl font-semibold">0</figure>
+                    <img src={star} alt="star" />
                   </div>
-                  <div className="title-border bg-[#fff] flex justify-between items-center">
+                  <div className="parent-border bg-[#fff] flex justify-between items-center">
                     <div className="flex flex-col">
-                      <h5>Attendance</h5>
-                      <figure>Total-100%</figure>
+                      <h5 className="text-[#000] text-xs font-medium font-secondary">Courses completed</h5>
+                      <figure className="text-[#000] font-bold text-base font-secondary">0</figure>
                     </div>
-                    <figure className="text-4xl font-semibold">0</figure>
+                    <img src={star} alt="star" />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="columns five bg-[red] ">5</div>
+            <div className="columns parent-border bg-[#fff] ">
+              <div className="flex gap-3">
+                <img src={events} alt="events" />
+                <h1 className="text-[#2B3F8C] text-base font-bold font-secondary">Events</h1>
+              </div>
+              <div className="flex justify-center items-center mx-0 inner-border-margin">
+                <h5 className="text-[#000] font-bold text-base font-secondary">You have no events coming up</h5>
+              </div>
+            </div>
           </main>
         </div>
       </div>
--- src/Components/Profilepage/Profilenavigation/sidebar.jsx
+++ src/Components/Profilepage/Profilenavigation/sidebar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { MdOutlineDashboard } from "react-icons/md";
 import { BsPerson } from "react-icons/bs";
 import { AiOutlineBook } from "react-icons/ai";
-import { GrAchievement } from "react-icons/gr";
+import achievement from "../../../assets/icons/hotel_class.svg";
 import { LuSettings } from "react-icons/lu";
 
 // const dashboardicon = <FaBeer />;
@@ -12,7 +12,7 @@ const sidebar = [
   { icon: <MdOutlineDashboard />, title: "Dashboard" },
   { icon: <BsPerson />, title: "Personal" },
   { icon: <AiOutlineBook />, title: "Courses" },
-  { icon: <GrAchievement />, title: "Achievements" },
+  { icon: <img src={achievement} alt="achievement-icon" />, title: "Achievements" },
   { icon: <LuSettings />, title: "Settings" },
 ];
 
@@ -35,7 +35,7 @@ const Sidebar = () => {
                 {item.icon}
               </i>
             </div>
-            <div className="text-[#001975]">
+            <div className="text-[#001975] font-semibold">
               <p>{item.title}</p>
             </div>
           </div>
