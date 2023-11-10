13225d05150dfe4e46f1f1cb5a46af3fa05b59a3
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-11-10 23:24:16 +0100

    Sign In Page Completed

--- src/Components/Sign In Page/sign-in.jsx
+++ src/Components/Sign In Page/sign-in.jsx
@@ -4,9 +4,33 @@ const SignIn = () => {
      return (
        <>
          <div className="sign-in">
-          <div className="image">
-               <img src="../../../src/assets/images/Logo-Sign-In.png" alt="" />
-          </div>
+               <div className="image">
+                    <img src="../../../src/assets/images/Logo-Sign-In.png" alt="" />
+               </div>
+               <div className="email">
+                    <p className="email-text">Email address</p>
+                    <div className="email-input">
+                         <input type="email" placeholder="Enter email" />
+                    </div>
+               </div>
+               <div className="password">
+                    <p className="password-text">Password</p>
+                    <div className="password-input">
+                         <input type="password" placeholder="Enter password" />
+                    </div>
+               </div>
+               <div className="login-button">
+                    <button type="submit">Log In</button>
+               </div>
+               <div className="sign-up">
+                    <p>Don't have an account? <a href="#">Sign Up</a></p>
+               </div>
+               <div className="line">
+                    <img src="../../../src/assets/images/line.png" alt="" />
+               </div>
+               <div className="google">
+                    <a href="#"><img src="../../../src/assets/images/google-logo.png" alt="" /></a>
+               </div>
          </div>
        </>
      );
