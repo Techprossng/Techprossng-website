caf9bd7c2c4bbcb7b40258a65c6fb4db25e40d9c
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-11-10 21:28:57 +0100

    sign up page starting

---
+++ src/Components/Sign In Page/sign-in.jsx
@@ -0,0 +1,14 @@
+import "../../Components/Sign In Page/Style/sign-in.css"
+
+const SignIn = () => {
+     return (
+       <>
+         <div className="sign-up">
+          <div className="image">
+               <img src="../../../src/assets/images/Logo-Sign-Up.png" alt="" />
+          </div>
+         </div>
+       </>
+     );
+   };
+ export default SignIn;
