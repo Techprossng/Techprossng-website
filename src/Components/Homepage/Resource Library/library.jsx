11e44946177a842c4af47e2c424c49712c21ba45
Benjamin Fadipe <fadipemoyinoluwa2016@gmail.com>
2023-11-13 17:11:24 +0100

    commit

--- src/Components/Homepage/Resource Library/library.jsx
+++ src/Components/Homepage/Resource Library/library.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import "../Resource Library/Style/library.css";
+import image1 from "../../../assets/images/image1.png";
+import image4 from "../../../assets/images/image4.png";
+import image5 from "../../../assets/images/image5.png";
+import image6 from "../../../assets/images/image6.png"
+
 
 function Library() {
   return (
@@ -7,22 +12,22 @@ function Library() {
         <h1 className="heading">Resource Library <div class="resourse-link"><a href="#">View Resource Library</a> <i class="fa-solid fa-arrow-right-long"></i></div></h1> 
         <div className="box-container">
             <div className="box">
-                <img src="../../../src/assets/images/image1.png" alt=""/>
+                <img src={image1} alt=""/>
                 <h3>Videos</h3>
                 <a href="#" class="btn">Explore Now</a>
             </div>
             <div className="box">
-                <img src="../../../src/assets/images/image4.png" alt="" />
+                <img src={image4}alt="" />
                 <h3>E-Books</h3>
                 <a href="#" class="btn">Explore Now</a>
             </div>
             <div className="box">
-                <img src="../../../src/assets/images/image5.png" alt=""/>
+                <img src={image5} alt=""/>
                 <h3>Projects</h3>
                 <a href="#" class="btn">Explore Now</a>
             </div>
             <div className="box">
-                <img src="../../../src/assets/images/image6.png" alt=""/>
+                <img src={image6} alt=""/>
                 <h3>Tutorials</h3>
                 <a href="#" class="btn">Explore Now</a>
             </div>
