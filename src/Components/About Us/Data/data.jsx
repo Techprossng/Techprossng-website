ac0ce0283fe646b073eda05a1623768e6b0b161d
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-11-17 23:18:35 +0100

    about us blog section

---
+++ src/Components/About Us/Data/data.jsx
@@ -0,0 +1,24 @@
+import BlogVector from "../../../assets/images/blog-frame.png"
+
+export const Blogs = [{
+     img: BlogVector, 
+     title: "TECHNOLOGY",
+     description: "Lorem ipsum dolor sit Lorem ipsum Lorem ipsum dolor.",
+     author: "Eze Chiemele",
+     date: "25th October, 2023"
+},
+{
+     img: BlogVector, 
+     title: "TECHNOLOGY",
+     description: "Lorem ipsum dolor sit Lorem ipsum Lorem ipsum dolor.",
+     author: "Eze Chiemele",
+     date: "25th October, 2023"
+},
+{
+     img: BlogVector, 
+     title: "TECHNOLOGY",
+     description: "Lorem ipsum dolor sit Lorem ipsum Lorem ipsum dolor.",
+     author: "Eze Chiemele",
+     date: "25th October, 2023"
+},
+]
--- src/Components/About Us/about-us.jsx
+++ src/Components/About Us/about-us.jsx
@@ -8,6 +8,10 @@ import MS from "../../assets/images/team/adeyemo-ms.jpg"
 import FM from "../../assets/images/team/chinyere-fm.jpg"
 import PM from "../../assets/images/team/mofe-pm.jpg"
 import COO from "../../assets/images/team/praise-coo.jpg"
+import BlogVector from "../../assets/images/blog-frame.png"
+import { FaLongArrowAltRight } from "react-icons/fa"; 
+import { Blogs } from "./Data/data";
+
 const AboutUs = () => {
     return (
       <>
@@ -80,171 +84,124 @@ const AboutUs = () => {
             </div>
           </div>
 
-          <div className="why-us flex gap-10">
-            <div className="impact-video ml-20">
-              <div className="thetext">
-                <h2>Why Choose Us?</h2>
-                <p>Elevate your learning journey with our innovative edtech <br />solutions. Expert educators, 
+          <div className="why-us flex gap-1 w-[100%]">
+            <div className="impact-video ml-20 w-[50%]">
+              <div className="thetext mt-8">
+                <h4 className="font-bold text-3xl text-[#001562]">Why Choose Us?</h4>
+                <p className="font-normal py-5 text-base">Elevate your learning journey with our innovative edtech <br />solutions. Expert educators, 
                   personalized learning paths, <br />and cutting-edge technology ensure a transformative <br />
                   educational experience tailored to your success.</p>
                 </div>
-              <div className="theimage">
-                <a href=""><img src={ImpactVideo} alt="" /></a>
+              <div className="thevideo w-full">
+                <a href=""><img src={ImpactVideo} alt="impact video" className="w-[90%]" /></a>
               </div>
             </div>
+    
             <div className="merit flex flex-col w-[50%] gap-y-10" style={{backgroundColor: "#000517"}}>
-              <div className="meritlist flex m-3 gap-x-3">
-                <div className="merigimgage flex">
+              <div className="meritlist flex items-center gap-10 mx-10 mt-5 text-justify">
+                <div className="meritimage">
                   <img src={Merit} alt="" />
                 </div>
-                <div className="merittext flex flex-col">
-                  <div className="merith5">
+                <div>
                   <h5 className="font-normal text-2xl text-[#fff]">Training</h5>
-                  </div>
-                  <div className="meritp">
                   <p className="font-medium text-base text-[#fff]">Proper Tech Learning-drill: We focus on real-world applications, 
                     so you can confidently apply your skills in any tech-related field.
                   </p>
-                  </div>
                 </div>
               </div>
-              <div className="meritlist flex m-3 gap-x-3">
-                <div className="merigimgage flex">
+              <div className="meritlist flex items-center gap-10 mx-10 text-justify">
+                <div className="meritimage">
                   <img src={Merit} alt="" />
                 </div>
-                <div className="merittext">
-                  <div className="merith5">
-                  <h5 className="font-normal text-base text-[#fff] ">Flexibility</h5>
-                  </div>
-                  <div className="meritp text-[#fff] ">
-                  <p>Learn at your own pace with our online and offline classes. Fit 
-                    tech education into your busy life.
+                <div>
+                  <h5 className="font-normal text-2xl text-[#fff]">Flexibility</h5>
+                  <p className="font-medium text-base text-[#fff]">Learn at your pace with our online & offline classes. Fit 
+                    tech education into your busy life without having to worry about missing out.
                   </p>
-                  </div>
                 </div>
               </div>
-              <div className="meritlist flex m-3 gap-x-3">
-                <div className="merigimgage flex">
+              <div className="meritlist flex items-center gap-10 mx-10 text-justify">
+                <div className="meritimage">
                   <img src={Merit} alt="" />
                 </div>
-                <div className="merittext">
-                  <div className="merith5">
-                  <h5 className="font-normal text-base text-[#fff] ">Industry-Driven Curriculum</h5>
-                  </div>
-                  <div className="meritp text-[#fff] ">
-                  <p>Our courses are designed in collaboration with industry experts to 
-                    ensure you're learning what matters most.
+                <div>
+                  <h5 className="font-normal text-2xl text-[#fff]">Industry-Driven Curriculum</h5>
+                  <p className="font-medium text-base text-[#fff]">Our courses are designed in collaboration with industry experts to 
+                    ensure you're learning what matters most with new technologies.
                   </p>
-                  </div>
                 </div>
               </div>
-              <div className="meritlist flex m-3 gap-x-3">
-                <div className="merigimgage flex ">
+              <div className="meritlist flex items-center gap-10 mx-10 text-justify mb-10">
+                <div className="meritimage">
                   <img src={Merit} alt="" />
                 </div>
-                <div className="merittext">
-                  <div className="merith5">
-                  <h5 className="font-normal text-base text-[#fff] ">Career Opportunities</h5>
-                  </div>
-                  <div className="meritp text-[#fff] ">
-                  <p>TechProsNaija graduates are followed up and given internship opportunities 
-                    to land their first jobs
+                <div>
+                  <h5 className="font-normal text-2xl text-[#fff]">Career Opportunities</h5>
+                  <p className="font-medium text-base text-[#fff]">TechProsNaija graduates are followed up and given internship opportunities 
+                    to land their first jobs which stands them out from other.
                   </p>
-                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="our-team">
-            <h1>Our Team</h1>
-            <div className="team-image">
-              <div className="set">
-                <div className="set1 ">
-                  <img src={MS} alt="" /><h2>Adeyemo Temitope</h2><h6>Marketing Specialist</h6>
-                </div>
-                <div className="set1">
-                  <img src={FM} alt="" /><h2>Chinyere Irechukwu</h2><h6>Finance Manager</h6>
-                </div>
-                <div className="set1">
-                  <img src={PM} alt="" /><h2>Mofeoluwa Opadotun</h2><h6>Project Manager</h6>
-                </div>
-              </div>
-              <div className="set">
-                <div className="set1">
-                  <img src={HR} alt="" /><h2>Sophia Runsewe</h2><h6>HR Manager</h6>
-                </div>
-                <div className="set1">
-                  <img src={COO} alt="" /><h2>Praise Olakanmi</h2><h6>COO</h6>
-                </div>
-                <div className="set1">
-                  <img src={CEO} alt="" /><h2>Sogelola Mayomiwa</h2><h6>CEO</h6>
-                </div>
-              </div>
+          <div className="blog-section flex flex-col items-center">
+            <div className="blog-header text-[#001562] mt-10">
+              <h4 className="font-bold text-3xl">
+                Our Blog
+              </h4>
             </div>
-          </div>
-          <div className="blog">
-            <h1>Our Blog</h1>
-            <div className="blog-cards">
-            <div className="blog-card">
-              <div className="blog-photo">
-                <img src="../../../src/assets/images/blog-frame.png" alt="" />
-              </div>
-              <div className="blog-topic">
-                <h4>Technology</h4>
+            <div className="blog-detail-container flex gap- bg-[aqua] ">
+              {Blogs.map((blog, index)=>(
+                <div className="blog-details mt-10 flex flex-col gap-3">
+                <div className="blog-poster">
+                  <img src={blog.img} alt="blog-poster" />
               </div>
               <div className="blog-text">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est!</p>
+                <p className="font-bold text-sm text-[#001562]">
+                  {blog.title}
+                </p>
+                <h5 className="font-bold text-2xl mt-3">
+                  {blog.description}
+                </h5>
               </div>
-              <div className="author">
-                <div className="name">
-                  <h6>Eze Chiemele</h6>
-                </div>
-                <div className="date">
-                  <h6>October 25, 2023</h6>
-                </div>
+              <div className="blog-author flex justify-between">
+              <p className="font-bold text-sm">
+                  {blog.author}
+                </p>
+                <p className="font-normal text-sm">
+                  {blog.date}
+                </p>
               </div>
-            </div>
-            <div className="blog-card">
-              <div className="blog-photo">
-                <img src="../../../src/assets/images/blog-frame.png" alt="" />
               </div>
-              <div className="blog-topic">
-                <h4>Technology</h4>
+              ))}
               </div>
-              <div className="blog-text">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est!</p>
-              </div>
-              <div className="author">
-                <div className="name">
-                  <h6>Eze Chiemele</h6>
-                </div>
-                <div className="date">
-                  <h6>October 25, 2023</h6>
-                </div>
-              </div>
-            </div>
-            <div className="blog-card">
-              <div className="blog-photo">
-                <img src="../../../src/assets/images/blog-frame.png" alt="" />
-              </div>
-              <div className="blog-topic">
-                <h4>Technology</h4>
+            {/* <div className="blog-details mt-10 flex flex-col gap-3">
+              <div className="blog-poster">
+                <img src={BlogVector} alt="blog-poster" />
               </div>
               <div className="blog-text">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est!</p>
+                <p className="font-bold text-sm text-[#001562]">
+                  TECHNOLOGY
+                </p>
+                <h5 className="font-bold text-2xl mt-3">
+                  Lorem ipsum dolor sit. <br />
+                  Lorem, ipsum. <br />
+                  Lorem, ipsum dolor. <br />
+                </h5>
               </div>
-              <div className="author">
-                <div className="name">
-                  <h6>Eze Chiemele</h6>
-                </div>
-                <div className="date">
-                  <h6>October 25, 2023</h6>
-                </div>
+              <div className="blog-author flex justify-between">
+              <p className="font-bold text-sm">
+                  Eze Chiemele
+                </p>
+                <p className="font-normal text-sm">
+                  October 25, 2023
+                </p>
               </div>
-            </div>
-            </div>
-            <div className="blog-link">
-              <a href="#">Go To Blog <i class="fa-solid fa-arrow-right-long"></i></a>
+            </div> */}
+            
+            <div className="blog-link w-10/12 mt-10 border-3 flex gap-10 justify-center items-center font-bold text-sm">
+              <a href="#">Go To Blog</a>
+              <FaLongArrowAltRight className="blogpoint"/>
             </div>
           </div>
           <div className="contact-us">
