cff457e297d2f01d67022356fcea9225997b6d08
Michael <madetunji1376@gmail.com>
2023-11-22 16:54:31 +0100

    contact form estructure

--- src/Components/Contact Us/contact-us.jsx
+++ src/Components/Contact Us/contact-us.jsx
@@ -2,74 +2,80 @@ import "../Contact Us/Style/contact-us.css"
 
 function ContactUs() {
   return (
-    <div className="contact-section">
-     <form action="" className="contact-form my-10">
-          <div className="contact-us m-10">
-            <div className="contact-intro flex flex-col justify-center items-center">
-              <h4 className="font-bold text-3xl text-[#001562] font-primary">Contact Us</h4>
-              <p className="font-medium text-base text-[#272727] mt-5 font-secondary">
-                Please fill out the form below, and our team will get back to
-                you as soon as possible.
-              </p>
-            </div>
-            <div className="forminputs font-secondary">
-              <div className="name flex justify-around items-center mt-10">
-                <div className="firstinput">
-                  <p className="text-[#272727] font-medium text-sm">
-                    First Name
-                  </p>
-                  <input type="name" className="" />
-                </div>
-                <div className="lastinput">
-                  <p className="text-[#272727] font-medium text-sm">
-                    Last Name
-                  </p>
-                  <input type="name" className="" />
-                </div>
-              </div>
-              <div className="course-option flex flex-col justify-center items-center mt-10">
-                <p className="text-[#272727] font-medium text-sm">
-                  Please select your course of enquiry.
-                </p>
-                <div className="course-select">
-                  <select id="courses">
-                    <option value="---">--Select One--</option>
-                    <option value="digital-marketing">Digital Marketing</option>
-                    <option value="data-analyst">Data Analyst</option>
-                    <option value="frontend">Frontend Web Developer</option>
-                    <option value="backend">Backend Web Developer</option>
-                  </select>
-                </div>
-              </div>
-              <div className="text-enquiry flex flex-col justify-center items-center mt-10">
-                <p className="text-[#272727] font-medium text-sm text-">
-                  How can we help?
-                </p>
-                <div className="field">
-                  <input type="name" className="" />
-                </div>
-              </div>
-              <div className="mail-website flex justify-around items-center mt-10">
-                <div className="mailinput">
-                  <p className="text-[#272727] font-medium text-sm">Email</p>
-                  <input type="email" className="" />
-                </div>
-                <div className="websiteinput">
-                  <p className="text-[#272727] font-medium text-sm">
-                    Website (Optional)
-                  </p>
-                  <input type="text" className="" />
-                </div>
-              </div>
-            </div>
-            <div className="submit-form flex justify-center mt-10  font-secondary">
-              <button type="submit">
-                <p className="font-semibold text-sm">Send</p>
-              </button>
+    <div  className="contact-form my-10">
+    <div className="contact-us m-10">
+      <div className="contact-intro flex flex-col justify-center items-center">
+        <h4 className="font-bold text-3xl text-[#001562]">Contact Us</h4>
+        <p className="font-medium text-base text-[#272727] mt-5">
+          Please fill out the form below, and our team will get back to
+          you as soon as possible.
+        </p>
+      </div>
+      <form action="" className="forminputs">
+        <div className="name flex justify-around items-center mt-10">
+          <div className="firstinput flex flex-col">
+            <label className="text-[#272727] font-medium text-sm">
+              First Name
+            </label>
+            <input type="name" className="" />
+          </div>
+          <div className="lastinput flex flex-col">
+            <label className="text-[#272727] font-medium text-sm">
+              Last Name
+            </label>
+            <input type="name" className="" />
+          </div>
+        </div>
+        <div className="mail-website flex justify-around items-center mt-10">
+          <div className="mailinput flex flex-col">
+            <label className="text-[#272727] font-medium text-sm">
+              Email
+            </label>
+            <input type="email" className="" />
+          </div>
+          <div className="websiteinput flex flex-col">
+            <label className="text-[#272727] font-medium text-sm">
+              Website (Optional)
+            </label>
+            <input type="text" className="" />
+          </div>
+        </div>
+        <div className="flex justify-center ">
+          <div className="course-option mt-10">
+            <label className="text-[#272727] font-medium text-sm">
+              Please select your course of enquiry.
+            </label>
+            <div className="course-select">
+              <select id="courses">
+                <option value="---">--Select One--</option>
+                <option value="digital-marketing">
+                  Digital Marketing
+                </option>
+                <option value="data-analyst">Data Analyst</option>
+                <option value="frontend">Frontend Web Developer</option>
+                <option value="backend">Backend Web Developer</option>
+              </select>
             </div>
           </div>
-        </form>
+        </div>
+        <div className="flex justify-center">
+        <div className="text-enquiry mt-10">
+          <label className="text-[#272727] font-medium text-sm text-">
+            How can we help?
+          </label>
+          <div className="field">
+            <input type="name" className="" />
+          </div>
+        </div>
+        </div>
+        <div className="submit-form flex justify-center mt-10">
+        <button type="submit">
+          <p className="font-semibold text-sm">Send</p>
+        </button>
+      </div>
+      </form>
     </div>
+  </div>
   )
 }
 
