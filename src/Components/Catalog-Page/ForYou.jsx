5c7364806eccefc537a286a65b3dd411936ddff6
CodeMaestroPro <141797744+CodeMaestroPro@users.noreply.github.com>
2023-12-06 00:53:46 +0100

    Update ForYou.jsx
    Added images and changed description of text.

--- src/Components/Catalog-Page/ForYou.jsx
+++ src/Components/Catalog-Page/ForYou.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import CardImage from '../../assets/images/full-shot-man-experiencing-virtual-reality.jpg'
+import CyberImage from '../../assets/images/cyber-img.jpg'
+import FrontendImage from '../../assets/images/frontend-img.jpg'
 import CourseForYouCard from './CourseForYouCard'
 
 const ForYou = () => {
@@ -8,21 +10,34 @@ const ForYou = () => {
       <div className="font-sans text-2xl  ml-8 mb-8">
         <p>Recommended For You</p>
       </div>
+<div className="mb-14 grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1">
+  <CourseForYouCard
+    key={0}
+    src={CardImage}
+    head={'UI/UX Design'}
+    text={
+      'Design user interface that does not only look great but also provide seamless and intuitive experiences for users.'
+    }
+  />
+  <CourseForYouCard
+    key={1}
+    src={CyberImage}
+    head={'Cyber Security'}
+    text={
+      '
+Cybersecurity is the practice of protecting computer systems, networks, and data from digital threats and attacks.'
+    }
+  />
+  <CourseForYouCard
+    key={2}
+    src={FrontendImage}
+    head={'Frontend Development'}
+    text={'
+Elevate your digital presence by mastering frontend development—cultivate user-friendly interfaces and enhance online experiences. '}
+  />
+</div>
 
-      <div className={`mb-14 grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1`}>
-        {[...Array.from({ length: 3 }, (_, i) => i * 2)].map((val, indx) => {
-          return (
-            <CourseForYouCard
-              key={indx}
-              src={CardImage}
-              head={'UI/UX Design'}
-              text={
-                'Design user interface that does not only look great but also provide seamless and intuitive experiences for users.'
-              }
-            />
-          )
-        })}
-      </div>
+     
     </section>
   )
 }
