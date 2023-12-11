a446d746f6f7122241948cdc06067146a0ca756f
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-12-11 22:02:44 +0100

    pricing

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import CourseCard from "../../assets/images/coursecard.png"
+import RatingStar from "../../assets/images/catalog-img/rating-stars.png"
+import { MdOutlineBookmarks } from "react-icons/md";
 
 const Pricing = () => {
   return (
     <>
       <div className="pricing-section mt-48">
         <div>
-          <div className="pricing-title flex flex-col justify-center items-center gap-4">
+          <div className="pricing-title flex flex-col justify-center items-center gap-4 my-8">
             <h3 className='font-primary font-normal text-5xl'>
               Pricing
             </h3>
@@ -14,7 +17,42 @@ const Pricing = () => {
             </p>
           </div>
           <div className='pricing-card-house'>
-
+            <div>
+              <div className='rounded-2xl'>
+                <div>
+                  <img src={CourseCard} alt="course image" />
+                </div>
+                <div>
+                  <h5 className='font-primary font-bold text-2xl text-[#001975] w-[528px] h-[29px]'>
+                    UI/UX Design
+                  </h5>
+                  <p className='font-secondary font-medium text-base text-[#656565] w-[528px] h-[42px]'>
+                    Design user interfaces that not only look great but also provide seamless and intuitive experiences for users.
+                  </p>
+                </div>
+                <div className='flex gap-2'>
+                  <div className='flex gap-1'>
+                    <img src={RatingStar} alt="" />
+                    <img src={RatingStar} alt="" />
+                    <img src={RatingStar} alt="" />
+                    <img src={RatingStar} alt="" />
+                    <img src={RatingStar} alt="" />
+                  </div>
+                  <div className='flex justify-center items-center gap-1'>
+                    <p className='font-primary font-medium text-base'>
+                      5.0
+                    </p>
+                    <p className='font-primary font-bold text-xs'>
+                      (3)
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <button type='submit' className='courseflex items-center'><MdOutlineBookmarks />Add To List</button>
+                  <button type='submit' className='flex items-center'><MdOutlineBookmarks />N5,000</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
