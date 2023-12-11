50e21fdffa50198f9d7aaf89a8ce43fa0c700acf
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-12-11 10:12:25 +0100

    pricing page commencement

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
-const Sample = () => {
+const Pricing = () => {
   return (
     <>
-      <div className="sample">
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vel
-          voluptas architecto laboriosam? Sed incidunt aspernatur quisquam fugit
-          tempora. Libero, magni. Qui quas ratione quae error minus eaque
-          dolore? Quibusdam.
-        </h1>
+      <div className="pricing-section mt-48">
+        <div>
+          <div className="pricing-title flex flex-col justify-center items-center gap-4">
+            <h3 className='font-primary font-normal text-5xl'>
+              Pricing
+            </h3>
+            <p className='font-secondary font-normal text-base text-[#555555] w-[684px] h-[38px]'>
+              Lorem ipsum dolor sit amet consectetur. Lobortis condimentum proin tempus et urna imperdiet ullamcorper placerat lacus.
+            </p>
+          </div>
+          <div className='pricing-card-house'>
+
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
-export default Sample;
+export default Pricing;
