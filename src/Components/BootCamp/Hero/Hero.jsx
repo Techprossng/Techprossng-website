c1f44a510e6d51afdadff900169f160979b8acf7
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2024-01-23 17:26:07 +0100

    hero bootcamp set up

---
+++ src/Components/BootCamp/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Hero/Style/Hero.css";
+
+function HeroBootcamp() {
+     return (
+          <div className='px-6 py-12 md:px-12 mt-24'>
+               
+          </div>
+     )
+}
+
+export default HeroBootcamp
--- src/pages/Bootcamp/bootcamp.jsx
+++ src/pages/Bootcamp/bootcamp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../../Components/Landingpage/Navbar/navbar';
+import HeroBootcamp from '../../Components/BootCamp/Hero/Hero';
 import AboutBootcamp from '../../Components/BootCamp/About/About';
 import Pricing from '../../Components/BootCamp/Pricing/pricing';
 import Footer from '../../Components/Landingpage/Footer/footer';
@@ -8,6 +9,7 @@ const BootCamp = () => {
   return (
     <>
     <Navbar />
+    <HeroBootcamp />
     <AboutBootcamp />
     <Pricing/>
     <Footer />
