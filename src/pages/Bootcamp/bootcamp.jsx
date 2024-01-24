90014fbdc6c31774bfd2a5cdbaaf9ca72d2b5298
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2024-01-24 14:02:31 +0100

    faq-section

--- src/pages/Bootcamp/bootcamp.jsx
+++ src/pages/Bootcamp/bootcamp.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import Navbar from '../../Components/Landingpage/Navbar/navbar';
-import AboutBootcamp from '../../Components/BootCamp/About/About';
-import Pricing from '../../Components/BootCamp/Pricing/pricing';
-import Footer from '../../Components/Landingpage/Footer/footer';
+import React from "react";
+import Navbar from "../../Components/Landingpage/Navbar/navbar";
+import AboutBootcamp from "../../Components/BootCamp/About/About";
+import Pricing from "../../Components/BootCamp/Pricing/pricing";
+import Footer from "../../Components/Landingpage/Footer/footer";
+import FaqBootcamp from "../../Components/BootCamp/FAQ/faq";
 
 const BootCamp = () => {
   return (
     <>
-    <Navbar />
-    <AboutBootcamp />
-    <Pricing/>
-    <Footer />
+      <Navbar />
+      <AboutBootcamp />
+      <Pricing />
+      <FaqBootcamp />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default BootCamp;
