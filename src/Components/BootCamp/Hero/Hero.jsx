8767c4ea46cba297be8d9964509ddb68212334bf
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2024-01-30 16:37:49 +0100

    UI Changes made to desktop view of hero sectin and bootcamp benefits

--- src/Components/BootCamp/Hero/Hero.jsx
+++ src/Components/BootCamp/Hero/Hero.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
 import "../Hero/Style/Hero.css";
 import { FaRegCalendarCheck } from "react-icons/fa6";
+import HeroImage from "../Hero/Style/hero1.svg"
 
 
 function HeroBootcamp() {
      return (
           <div className='hero-bootcamp px-6 py-12'>
-               <div className='mt-32  items-center text-[#fff]'>
-                  <div className='flex justify-center'>
-                  <div className='date flex gap-[5px] w-[400px] h-[50px] p-[14px]'>
-                         <FaRegCalendarCheck size={20}/>
-                         <h5 className='font-primary text-[16px] font-semibold '>STARTING: MONDAY, 4TH MARCH, 2024</h5>
+               <div className='mt-32 flex gap-5 items-center text-[#000]'>
+                    <div className='flex flex-col gap-4 w-[45%]'>
+                         <div className='date flex justify-center gap-[5px] h-[50px] p-[14px]'>
+                              <FaRegCalendarCheck size={20}/>
+                              <h5 className='font-secondary text-[16px] font-semibold '>STARTING: MONDAY, 4TH MARCH, 2024</h5>
+                         </div>
+                         <div>
+                              <div className='hero-date  text-center'> 
+                                   <h4 className='text-[55px] text-center font-primary font-bold'> Tech Career</h4>
+                                   <h4 className='text-[55px] text-center font-primary font-bold'> Launcpad</h4> 
+                              </div>
+                              <div className='pt-8  text-center items-center hero-description gap-2'>
+                                   <p className='text-[24px]'> Come and invest in your future by getting into tech with 
+                                   our 2-weeks power loaded bootcamp for every one inclusive </p>
+                              </div>
+                         </div>
+                         <div className='flex justify-center'>
+                              <button className='hero-register  w-[206px] h-[52px] px-5 py-3 mt-4'>
+                              <a href="#">Register</a>
+                              </button>
+                         </div>
                     </div>
-                  </div>
                     <div>
-                         <div className='hero-date  text-center'> 
-                              <h4 className='text-[80px] text-center font-primary font-bold'> 2 WEEKS<span className='text-[80px] font-secondary font-normal'>BOOTCAMP</span> </h4> 
-                         </div>
-                         <div className='pt-8  text-center items-center hero-description gap-2'>
-                              <p className='text-[24px]'> Come and invest in your future by getting into tech with our 2-weeks power<br className='line-break-3'/>
-                              loaded bootcamp for every one inclusive </p>
-                         </div>
-                    </div>
-                    <div className='flex justify-center'>
-                    <button className='hero-register  w-[206px] h-[52px] px-5 py-3 mt-16'>
-                         <a href="#">Register</a>
-                    </button>
-                    </div>
+                         <img src={HeroImage} alt="Hero image people learning" />
+                    </div>                    
                </div>
           </div>
      )
